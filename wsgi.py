277ac5ecff9972ce70a6ddb3a3126bc465510c1c
Shahrukh Khan <sk28671@gmail.com>
2020-05-29 22:26:16 +0500

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -45,9 +45,6 @@ gauth.LoadCredentialsFile("mycreds.txt")
 if gauth.credentials is None:
     # Authenticate if they're not there
     gauth.LocalWebserverAuth()
-elif gauth.access_token_expired:
-    # Refresh them if expired
-    gauth.Refresh()
 else:
     # Initialize the saved creds
     gauth.Authorize()
